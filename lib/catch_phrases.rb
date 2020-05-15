cb8836f1ae3f68865baf4f9730d8c34ef2b96ea3
Zana Hertica <zhertica@gmail.com>
2020-05-15 19:59:15 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,10 +1,10 @@
-$status = 'Thank You Mario! But Our Princess Is In Another Castle!'
 def mario
-  
+  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   phrase = "It's-a me, Mario!"
   puts phrase
 end
 
 def toadstool
+  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
 end
